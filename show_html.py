075d237ded0f22c04a4f85e9102dc9f9f96cf60b
Alessandra Pascazio <alemidori777@gmail.com>
2016-06-02 22:18:45 +0200

    rimosso i commenti lunghissimi

--- show_html.py
+++ show_html.py
@@ -17,18 +17,6 @@ def handle_htmlpage():
     with open(html_path, "wb") as prova:
         prova.write(soup.prettify("utf-8"))
 
-    #visualizzare un racconto alla volta ricostruito partendo dalle posizioni nella collezione
-    #utilizzare un percentile per scremare i topic più rilevanti per un paragrafo
-    #associare ad ogni topic un colore randomico
-    #mostrare l'intero testo e sotto ogni paragrafo il colore del topic più rilevante
-    #mostrare in corrispondenza dei topic: il colore, il livello di presenza nell'intero racconto
-    #e la posizione all'interno del racconto stesso
-
-    #IN PRATICA
-    #prendi il massimo tra i valori della distribuzione dei documenti sui topic nella coppia(secondo valore) e
-    #inserisci in un array il valore dell'id del paragrafo e l'id del topic con  valore massimo
-    #effettuare un'interrogazione per cui per ogni documento txt si prende il testo vero e proprio
-    #sotto la voce "descr" e lo si visualizza ...
     return
 
 handle_htmlpage()
