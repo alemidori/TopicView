f5090853e9fc2b202c067fd22d4acc8f7f4dee47
Alessandra Pascazio <alemidori777@gmail.com>
2016-06-07 12:27:03 +0200

    visualizzo paragrafi di un intero racconto e topic su html però solo di un documento

--- main_show_topic_distribution.py
+++ main_show_topic_distribution.py
@@ -11,12 +11,14 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 list_main_topic = topic_distribution.calculate_main_topic_for_parag()
 
 #ATTENZIONE: QUANDO DO IL NUMERO DELLA POSIZIONE NON DEVO RIFERIRMI ALL'ATTRIBUTO POS MA ALLA POSIZIONE ALL'INTERO DELLA COLLECTION
-cursor = storage.paragraphs_coll.find({'id_story': '../texts/fpn-andrews-andrews.txt', 'pos':8}, {'_id':0,'descr':1})
-string = ""
+cursor = storage.paragraphs_coll.find({'id_story': '../texts/fpn-andrews-andrews.txt'}, {'_id':0,'descr':1})
+strings = []
 for k in cursor:
-    string = k['descr']
+    strings.append(k['descr'])
 
-show_html.handle_htmlpage(string, list_main_topic[7])
+for text in strings:
+    print("Scrittura paragrafo "+str(strings.index(text)) +" / "+str(len(strings)))
+    show_html.handle_htmlpage(text, list_main_topic[strings.index(text)])
 
 
 print("Processo terminato.")
--- show_html.py
+++ show_html.py
@@ -14,15 +14,29 @@ def handle_htmlpage(left, right):
 
     #serve a parsare l'html e a modificare i contenuti dei tag e gli eventuali attributi
     soup = BeautifulSoup(open(html_path), 'lxml')
-    contenuto = soup.p #mi riferisco al paragrafo dell'html attraverso il tag 'p'
+    table = soup.find('table', attrs={'id': 'topicshow'} )
+    #contenuto = soup.find('td', attrs={'id': 'textcontent'})
+    newrow = soup.new_tag("tr")
 
+    contenuto = soup.new_tag("td")
+    newrow.append(contenuto)
+
+    tag_p = soup.new_tag("p")
+    contenuto.append(tag_p)
+    tag_p.string = left
     # modifico il contenuto del tag con una nuova stringa
-    contenuto.string =  left
-    contenuto['style'] = "background-color:"+map_color[right]+";color:white"
-    topic = soup.h1
-    topic.string = str(right)
-    topic['style'] = "background-color:"+map_color[right]+";color:white"
+    tag_p['style'] = "background-color:"+map_color[right]+";color:white"
 
+    #topic_side = soup.find('td', attrs={'id': 'topictext'})
+    topic_side = soup.new_tag("td")
+    newrow.append(topic_side)
+    tag_h1 = soup.new_tag("h1")
+    topic_side.append(tag_h1)
+
+    tag_h1.string = "Topic numero " + str(right)
+    tag_h1['style'] = "background-color:"+map_color[right]+";color:white"
+
+    table.append(newrow)
 
     with open(html_path, "wb") as prova:
         prova.write(soup.prettify("utf-8"))
@@ -39,8 +53,3 @@ def set_topic_random_color():
         map_topic_color[i] = color
 
     return map_topic_color
-
-
-
-with open(html_path, "r") as prova:
-    print(prova.read())
